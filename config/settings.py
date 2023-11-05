e1a89ae2b95db904a924ffdfbead472da1d7cd9f
mohammad ghorashi <programmers.TM@gmail.com>
2023-11-05 22:08:43 +0330

    add user app

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # other app
-    'accounts.apps.AccountsConfig'
+    'accounts.apps.AccountsConfig',
+    'user.apps.UserConfig',
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('', TemplateView.as_view(template_name='pages/home.html'), name='home')
+    path('user/', include('user.urls')),
+    path('', TemplateView.as_view(template_name='pages/home.html'), name='home'),
 ]
