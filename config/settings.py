04c55db22126a9997d08a91621d42320fb67465c
mohammad ghorashi <mohammadghorashi81@gmail.com>
2023-12-09 21:53:15 +0330

    none

--- config/settings.py
+++ config/settings.py
@@ -59,7 +59,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR.joinpath('templates')
+            str(BASE_DIR.joinpath('templates')),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -121,9 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = BASE_DIR.joinpath('static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR.joinpath('static')]
+STATICFILES_DIRS = [BASE_DIR.joinpath('config/static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
